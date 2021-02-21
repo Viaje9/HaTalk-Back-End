cc37f527d41edffc1737a34db56d9fa910373881
Viaje9 <h99y5246cv@gmail.com>
2021-02-21 21:58:21 +0800

    移除

--- jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-
-// 設定密鑰
-const SECRET = 'thisismynewproject'
-// 建立 Token
-const token = jwt.sign({ _id: 'hello' }, SECRET, { expiresIn: '1 day' })
-
-console.log(token);
